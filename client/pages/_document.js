b3030e7cdc5fc3416b935c29922880e510e43047
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-04 18:05:44 +0100

    Fix bug with responsiveness on mobile devices

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -10,6 +10,12 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+          <meta charSet="utf-8"/>
+          <meta name="description" content=""/>
+          <meta name="keywords" content=""/>
+          
           <link rel='stylesheet' type='text/css' href='/static/css/bootstrap.min.css' />
           <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
         </Head>
