f2afac4b0c0a0589b58f29e2a751fc135ae43edc
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-04 18:01:27 +0100

    Remove authentication logic from boilerplate

--- api/graphql/schema/queries.js
+++ api/graphql/schema/queries.js
@@ -1,21 +1,10 @@
-// Get logic middleware
 const {
-  authAccess,
-} = require('../logic/authentication');
-
-const {
-  UserTC,
-  ViewerTC,
   PostTC,
 } = require('../composers');
 
 // Add fields and resolvers to rootQuery
 module.exports = {
-  posts: PostTC.getResolver('findMany'),
-  ...authAccess({ scope: 'User' }, {
-    candidateIsAuthenticated: UserTC.getResolver('isAuthenticated'),
-    Viewer: ViewerTC.getResolver('viewer'),
-  }),
+  posts: PostTC.getResolver('pagination'),
   currentTime: {
     type: 'Date',
     resolve: () => new Date().toISOString(),
